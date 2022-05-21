8e861c50287a9b19f976139049303d32f0deb5c1
Samuel Nicolau <muambalas@gmail.com>
2022-05-21 02:57:08 +0100

    add
    Novas atualizações

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,7 @@ const menuToggle = document.querySelector('#menu-toggle');
 const btnMobile = document.querySelector('.btn-mobile');
 const scrollBtn = document.querySelector('.btn-scroll');
 const cursor = document.querySelector('.cursor');
-        
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = (e.pageX - 10)+ 'px';
-    cursor.style.top = (e.pageY - 10)+ 'px';
-})
+     
 btnMobile.addEventListener('click', () => {
     const visibility = menuToggle.getAttribute('data-visible');
 
